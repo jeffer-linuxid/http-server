053527acaa0a78cdc0165f1bef28e8ff89de03c9
acai <acainaoterraser@gmail.com>
2026-08-16 12:59:16 -0300

    Minhas atualizações locais

--- c.c
+++ c.c
@@ -1,28 +1,34 @@
 
 
-
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 50
+
 int main(void){
 
-
-    int tcp_socket = socket(
+    char buffer[256] = {};
+    int socket_my = socket(
         AF_INET,
         SOCK_STREAM,0
     );
-    if(tcp_socket == -1){
+    if(socket_my < 0){
         perror("tcp_socket()");
-        return 1;
+        return -1
     }
-    struct c
+
+     struct sockaddr_in addr =
     {
-        /* data */
+       AF_INET,
+       0x901f,
+       0
     };
     
-    printf("Success make a socket")
+    printf("Success make a socket");
+    int client_connection = accept(socket_my,0,0);
+    bind(socket_my,&addr,sizeof(addr));
 
-
+    listen(socket_my,10);
+    recv(client_connection,buffer,256,0);
 
 }
