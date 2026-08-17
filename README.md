56515feceafb72f16b1428af84ae0b845448bfaa
acai <acainaoterraser@gmail.com>
2026-08-17 08:57:30 -0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # http-server
+guide link/links dos manuais:
+https://dev.to/jeffreythecoder/how-i-built-a-simple-http-server-from-scratch-using-c-739
+arch wiki and use  man to guide me / arch wiki e uso o man par me guiar
+https://man.archlinux.org/man/tcp.7.en
+*man 
--- c.c
+++ c.c
@@ -1,44 +1,78 @@
-
-
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 
-int main(void){
+int server_fd;
+int new_fd;
 
-    char buffer[256] = {};
-    int socket_my = socket(
-        AF_INET,
-        SOCK_STREAM,0
-    );
-    if(socket_my < 0){
-        perror("tcp_socket()");
-        return -1; 
+void *handle_client(void *arg) {
+    int client_fd = *(int *)arg;   // copia o valor ANTES que o main sobrescreva new_fd
+
+    char buffer[256] = {0};
+    read(client_fd, buffer, sizeof(buffer));
+    printf("Recebido: %s\n", buffer);
+
+    char *resposta = "Mensagem recebida!\n";
+    write(client_fd, resposta, strlen(resposta));
+
+    close(client_fd);
+    return NULL;
+}
+
+int main(void)
+{
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd < 0) {
+        perror("socket()");
+        return -1;
     }
 
-     struct sockaddr_in addr =
-    {
-       AF_INET,
-       0x901f,
-       0
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8080),
+        .sin_addr.s_addr = INADDR_ANY
     };
-    
-    printf("Success make a socket");
-    int client_connection = accept(socket_my,0,0);
-    if  (bind(socket_my,&addr,sizeof(addr)) < 0){
-        perror("bind(socket_my,&addr,sizeof(addr)) < 0");
-        return -1; 
+
+    printf("Success make a socket\n");
+
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("bind()");
+        return -1;
     }
 
-    if  (listen(socket_my,10) < -1){
-        printf("Not listen")
+    if (listen(server_fd, 10) < 0) {
+        perror("listen()");
+        return -1;
     }
+
+    printf("Waiting connection...\n");
+
     while (1)
     {
-        
-    }
-    
-    recv(client_connection,buffer,256,0);
+        struct sockaddr_in client_addr;
+        socklen_t addr_len = sizeof(client_addr);
 
+        new_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+
+        if (new_fd < 0) {
+           
+            perror("accept()");
+            continue;
+        }
+
+
+        printf("Client connected!\n");
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_client, &new_fd);
+        pthread_join(tid, NULL); 
+    }
+
+    close(server_fd);
+    return 0;
 }
