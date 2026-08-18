aca71f84905e1c170d2bdb685b1534b90706b0ff
Jeffe <acainaoterraser@gmail.com>
2026-08-18 10:58:00 -0300

    Update and rename c.c to server.c

--- server.c
+++ server.c
@@ -9,19 +9,6 @@
 int server_fd;
 int new_fd;
 
-void *handle_client(void *arg) {
-    int client_fd = *(int *)arg; 
-
-    char buffer[256] = {0};
-    read(client_fd, buffer, sizeof(buffer));
-    printf("Recebido: %s\n", buffer);
-
-    char *resposta = "Mensagem recebida!\n";
-    write(client_fd, resposta, strlen(resposta));
-
-    close(client_fd);
-    return NULL;
-}
 
 int main(void)
 {
