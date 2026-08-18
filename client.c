847ac5918059af5e8b7b11457f4c593e2e776921
Jeffe <acainaoterraser@gmail.com>
2026-08-18 13:48:24 -0300

    Update client.c

--- client.c
+++ client.c
@@ -5,7 +5,7 @@ void *handle_client(void *arg) {
     int client_fd = *(int *)arg; 
     char buffer[256] = {0};
     read(client_fd, buffer, sizeof(buffer));
-    printf("Recebido: %s\n", buffer);
+    printf("response: %s\n", buffer);
 
     char *response = "Mensagem recebida!\n";
     write(client_fd, resposta, strlen(resposta));
