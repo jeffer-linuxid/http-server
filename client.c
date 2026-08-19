600e45df8a63c1c361bbfaa0056676c2cd2605bb
acai <acainaoterraser@gmail.com>
2026-08-19 08:38:22 -0300

    .

--- client.c
+++ client.c
@@ -5,17 +5,14 @@ void *handle_client(void *arg) {
     int client_fd = *(int *)arg; 
     char buffer[256] = {0};
     read(client_fd, buffer, sizeof(buffer));
-    printf("response: %s\n", buffer);
+    printf("Receive: %s\n", buffer);
 
-    char *response = "Mensagem recebida!\n";
+    char *response = "Mensenge receive!\n";
     write(client_fd, response, strlen(response));
 
     close(client_fd);
-    return NULL;
-}
-
-
-int main(){
-
+    return 0;
     
 }
+
+
